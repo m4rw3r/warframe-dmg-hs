60b723fdf2575592a535a0ab7cc0b9e84e1ba7c5
Martin Wernstål <m4rw3r@gmail.com>
2014-09-14 19:00:48 +0200

    Generalized types of findMaximum and findMaximumN, added log-function

--- src/utils.hs
+++ src/utils.hs
@@ -1,8 +1,12 @@
 module Utils where
 
 import Data.List
+import Debug.Trace (trace)
 
-findMaximum :: (a -> Float) -> (Float, a) -> a -> (Float, a)
+lg :: Show a => a -> a
+lg a = trace (show a) a
+
+findMaximum :: (Num b, Ord b) => (a -> b) -> (b, a) -> a -> (b, a)
 findMaximum f (av, a) b = if bv > av then (bv, b) else (av, a)
     where
         bv = f b
@@ -14,7 +18,7 @@ cmpSnd :: Ord b => (a, b) -> (a, b) -> Ordering
 cmpSnd (_, a) (_, b) = compare a b
 
 -- Finds the N max values
-findMaximumN :: Int -> (a -> Float) -> [(Float, a)] -> a -> [(Float, a)]
+findMaximumN :: (Num b, Ord b) => Int -> (a -> b) -> [(b, a)] -> a -> [(b, a)]
 findMaximumN n f as b = take n vs
     where
         vs = sortBy (flip cmpFst) (as ++ [(f b, b)])
