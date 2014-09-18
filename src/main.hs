cd5ddf292e788ac8901bf9932bf898e1efcc32e7
Martin Wernstål <m4rw3r@gmail.com>
2014-09-18 17:28:45 +0200

    Started with GUI using Threepenny

--- src/main.hs
+++ src/main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RecursiveDo #-}
+
+import Control.Monad (void)
 import Data.List
+import Data.Maybe
+import Data.Monoid
 import Math.Combinat.Sets (choose)
-import qualified Numeric.Probability.Distribution as Dist
 import Text.Printf
+import qualified Numeric.Probability.Distribution as Dist
 
 import Damage
 import Mod
@@ -10,6 +15,9 @@ import Utils
 import qualified Rifles
 import qualified Pistols
 import qualified Shotguns
+
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core hiding (delete)
 
 mods :: [Mod]
 mods = forceModsByNames ["Serration", "Speed Trigger", "Split Chamber", "Heavy Caliber", "Stormbringer", "Infected Clip", "Point Strike", "Vital Sense"] Rifles.mods
@@ -35,8 +43,55 @@ probabilityToLine :: (Show a, Ord a) => Dist.T Float a -> String
 probabilityToLine = Dist.pretty (printf "%2.2f%%")
 
 main :: IO ()
-main = print "test"
-    >> putStrLn (probabilityToLine $ shotProbabilities l)
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup window = void $ mdo
+    return window # set title "Warframe Mod Optimizer"
+    
+    selType <- UI.select # set UI.name "weapon-type-select"
+                     # set UI.id_ "weapon-type-select" #+ [
+                         UI.option # set UI.selected True #+ [string "Rifle"], 
+                         UI.option #+ [string "Shotgun"],
+                         UI.option #+ [string "Pistol"]
+                         ]
+    selWeapon <- UI.select # set UI.name "weapon-select"
+                    # set UI.id_ "weapon-select" #+ (weaponOption <$> (weaponsOfType Rifle))
+    chosenType <- UI.p
+    
+    getBody window #+ [element selType, element chosenType, element selWeapon]
+    
+    -- events
+    let eType = UI.selectionChange selType
+    
+    -- behaviours
+    bWeapon <- fmap (selToWeaponType <$>) $ stepper (Just 0) eType
+    let cWeapons = (weaponsOfType) <$> bWeapon
+    
+    element chosenType # sink UI.text (show <$> bWeapon)
+    onChanges cWeapons $ \w -> do
+        let elems = (weaponOption <$> w)
+        element selWeapon # set UI.children [] #+ elems
+
+weaponOption :: Weapon -> UI Element
+weaponOption w = UI.option # set UI.id_ (name w) #+ [string $ name w]
+
+data WeaponType = Rifle | Shotgun | Pistol
+    deriving (Show)
+
+selToWeaponType :: Maybe Int -> WeaponType
+selToWeaponType (Just 0) = Rifle
+selToWeaponType (Just 1) = Shotgun
+selToWeaponType (Just 2) = Pistol
+selToWeaponType _ = Rifle
+
+weaponsOfType :: WeaponType -> [Weapon]
+weaponsOfType Rifle = [Rifles.lanka, Rifles.dread, Rifles.vulkar]
+weaponsOfType Shotgun = [Shotguns.drakgoonUncharged, Shotguns.drakgoonCharged]
+weaponsOfType Pistol  = [Pistols.despair]
+
+test :: IO ()
+test = putStrLn (probabilityToLine $ shotProbabilities l)
     >> print Rifles.lanka
     >> print (applyMods Rifles.vulkar mods)
     >> print l
