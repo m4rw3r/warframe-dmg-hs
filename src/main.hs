d00083406162959f409ce497751c015abd4cc1e2
Martin Wernstål <m4rw3r@gmail.com>
2014-09-19 16:23:35 +0200

    Added checkboxes for mods and accumulate the checked ones into a list

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecursiveDo #-}
 
-import Control.Monad (void)
+import Control.Monad (void, liftM)
 import Data.List
 import Data.Maybe
 import Data.Monoid
@@ -28,30 +28,92 @@ setup window = void $ mdo
     
     selType <- UI.select # set UI.name "weapon-type-select"
                      # set UI.id_ "weapon-type-select" #+ [
-                         UI.option # set UI.selected True #+ [string "Rifle"], 
+                         UI.option #+ [string "Rifle"], 
                          UI.option #+ [string "Shotgun"],
                          UI.option #+ [string "Pistol"]
                          ]
     selWeapon <- UI.select # set UI.name "weapon-select"
                     # set UI.id_ "weapon-select" #+ (weaponOption <$> (weaponsOfType Rifle))
+    selMods <- UI.div
+    chosenMods <- UI.ul
     chosenType <- UI.p
     
-    getBody window #+ [element selType, element chosenType, element selWeapon]
+    getBody window #+ [string "Weapon type: ",
+        element selType,
+        element chosenType,
+        string "Weapon: ",
+        element selWeapon,
+        string "Mods: ",
+        element selMods,
+        element chosenMods]
     
     -- events
     let eType = UI.selectionChange selType
     
     -- behaviours
     bWeapon <- fmap (selToWeaponType <$>) $ stepper (Just 0) eType
-    let cWeapons = (weaponsOfType) <$> bWeapon
+    let bWeaponList  = (weaponsOfType) <$> bWeapon
+        bModList     = (modsOfType)    <$> bWeapon
     
     element chosenType # sink UI.text (show <$> bWeapon)
-    onChanges cWeapons $ \w -> do
-        let elems = (weaponOption <$> w)
-        element selWeapon # set UI.children [] #+ elems
+    onChanges bWeapon $ \w -> do
+        (b, el) <- mkModList (modsOfType w)
+        element selMods # set UI.children [] #+ [element el]
+        onChanges b $ \w -> do
+            element chosenMods # set UI.children [] #+ (modListItem <$> w)
+    onChanges bWeaponList $ \w -> do
+        element selWeapon # set UI.children [] #+ (weaponOption <$> w)
+    -- onChanges bMods $ \m -> do
+    --    element selMods # set UI.children [] #+ (fmap fst) m
 
 weaponOption :: Weapon -> UI Element
 weaponOption w = UI.option # set UI.id_ (name w) #+ [string $ name w]
+
+modListItem :: ModChecked -> UI Element
+modListItem m = UI.li #+ [UI.p #+ [string $ modName (modData m)],
+                          UI.p #+ [string $ intercalate ", " (fmap show (modValues (modData m)))]]
+
+data ModChecked = ModChecked {
+    modData    :: Mod,
+    modChecked :: Bool
+    }
+    deriving (Show, Eq)
+
+accumModChecked :: [ModChecked] -> [ModChecked] -> [ModChecked]
+accumModChecked a b = filter modChecked (l ++ a)
+    where
+        l = deleteFirstsBy (\x y -> modData x == modData y) b a
+
+mkModList :: [Mod] -> UI (Behavior [ModChecked], Element)
+mkModList m = do
+    elWrapper <- UI.new
+    list <- sequence $ fmap mkModCheckbox m
+    let eMods   = unions (fmap fst list)
+        elBoxes = fmap snd list
+    bMods   <- accumB [] (fmap accumModChecked eMods)
+    
+    element elWrapper #+ fmap element elBoxes
+    
+    return (bMods, elWrapper)
+
+mkModCheckbox :: Mod -> UI (Event ModChecked, Element)
+mkModCheckbox m = do
+    elCheckbox <- UI.input # set UI.type_ "checkbox"
+    elLabel    <- UI.div #+ [mkElement "label" #+ [element elCheckbox, string (modName m)]]
+    
+    let eChecked = UI.checkedChange elCheckbox
+        eMod     = (fmap (\x -> case x of
+            True  -> ModChecked m True
+            False -> ModChecked m False) eChecked)
+   
+    return (eMod, elLabel)
+
+checkedModName :: Element -> UI String
+checkedModName e = do
+    v <- e # get UI.checked
+    case v of
+        True  -> e # get UI.value
+        False -> return ""
 
 data WeaponType = Rifle | Shotgun | Pistol
     deriving (Show)
@@ -62,7 +124,12 @@ selToWeaponType (Just 1) = Shotgun
 selToWeaponType (Just 2) = Pistol
 selToWeaponType _ = Rifle
 
+modsOfType :: WeaponType -> [Mod]
+modsOfType Rifle   = Rifles.mods
+modsOfType Shotgun = Shotguns.mods
+modsOfType Pistol  = Pistols.mods
+
 weaponsOfType :: WeaponType -> [Weapon]
-weaponsOfType Rifle = [Rifles.lanka, Rifles.dread, Rifles.vulkar]
+weaponsOfType Rifle   = [Rifles.lanka, Rifles.dread, Rifles.vulkar]
 weaponsOfType Shotgun = [Shotguns.drakgoonUncharged, Shotguns.drakgoonCharged]
 weaponsOfType Pistol  = [Pistols.despair]
